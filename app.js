39a8ec9e35634b95930295b8b098a41809b64562
hung230206 <hungtranbeta@gmail.com>
2026-07-08 07:23:02 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 
 const app = express();
 
+const contactRouter = require('./app/routes/contact.route');
+app.use('/api/contacts', contactRouter);
+
 app.use(cors());
 app.use(express.json());
 
